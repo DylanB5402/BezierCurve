5af9d5a68f85d9fbf4c32cac64e8d2dbe13314b3
Dylan Barva <d.barva5402@gmail.com>
2018-05-27 11:32:23 -0700

    separate bezier curves

--- src/BezierCurveCraigReynolds.java
+++ src/BezierCurveCraigReynolds.java
@@ -2,7 +2,9 @@ package src;
 
 import java.util.ArrayList;
 
-public class BezierCurve {
+/*Fancier Bezier Curve for craig reynold's path following, probably won't ever use it though*/
+public class BezierCurveCraigReynolds {
+	
 	private double m_x0;
 	private double m_y0;
 	private double m_x1;
@@ -21,18 +23,25 @@ public class BezierCurve {
 	private	ArrayList<Double> m_xList = new ArrayList<Double>();
 	private ArrayList<Double> m_yList = new ArrayList<Double>();
 	private ArrayList<Double> m_angleList = new ArrayList<Double>();
+	private ArrayList<Double> m_slopeList = new ArrayList<Double>();
+	private ArrayList<Double> m_yInterceptList = new ArrayList<Double>();
+	private ArrayList<Double> m_perpendicularSlopeList = new ArrayList<Double>();
+	private ArrayList<Double> m_hypotenuseList = new ArrayList<Double>();
+	private ArrayList<Double> m_deltaXList = new ArrayList<Double>();
 	private double m_deltaX;
 	private double m_deltaY;
 	private double m_prevX;
 	private double m_prevY;
 	private double m_distance;
 	private double m_hypotenuse;
-
+	private double m_slope;
+	private double m_yIntercept;
+	private double m_perpendicularSlope;
 	
 	/*
 	 * 
 	 */
-	public BezierCurve(double x0, double y0, double x1, double y1, double x2, double y2, double x3, double y3, double step ) {
+	public BezierCurveCraigReynolds(double x0, double y0, double x1, double y1, double x2, double y2, double x3, double y3, double step ) {
 		m_x0 = x0;
 		m_y0 = y0;
 		m_x1 = x1;
@@ -58,12 +67,31 @@ public class BezierCurve {
 			m_deltaY = m_y - m_prevY;			 
             m_angle = Math.atan2(m_deltaX, m_deltaY);
             m_angle = Math.toDegrees(m_angle);
-            m_hypotenuse = NerdyMath.distanceFormula(m_x, m_y, m_prevX, m_prevY);      
+            m_hypotenuse = NerdyMath.distanceFormula(m_x, m_y, m_prevX, m_prevY);
+            if (m_deltaX != 0) {
+            	m_slope = m_deltaY/m_deltaX;
+            }
+            else {
+            	m_slope = 0;
+            }
+            m_yIntercept = m_y - m_slope * m_x;
+            if (m_slope != 0) {
+            	m_perpendicularSlope = -Math.pow(m_slope, -1);
+            	
+            }
+            else {
+            	m_perpendicularSlope = 0;
+            }
             m_distance += m_hypotenuse;
             m_tList.add(m_t);
             m_xList.add(m_x);
             m_yList.add(m_y);
             m_angleList.add(m_angle);
+            m_slopeList.add(m_slope);
+            m_yInterceptList.add(m_yIntercept);
+            m_perpendicularSlopeList.add(m_perpendicularSlope);
+            m_hypotenuseList.add(m_hypotenuse);
+            m_deltaXList.add(m_deltaX);
             m_prevX = m_x;
             m_prevY = m_y;
             m_a += 1;
@@ -95,6 +123,26 @@ public class BezierCurve {
 		return m_angleList.get(t);
 	}
 	
+	public double getSlope(int t) {
+		return m_slopeList.get(t);
+	}
+	
+	public double getYIntercept(int t) {
+		return m_yInterceptList.get(t);
+	}
+	
+	public double getPerpendicularSlope(int t) {
+		return m_perpendicularSlopeList.get(t);
+	}
+	
+	public double getHypotenuse(int t) {
+		return m_hypotenuseList.get(t);
+	}
+	
+	public double getDeltaX(int t) {
+		return m_deltaXList.get(t);
+	}
+	
 	public double getCurveLength() {
 		return m_distance;
 	}
@@ -117,4 +165,3 @@ public class BezierCurve {
 		}
 	}
 }
-
